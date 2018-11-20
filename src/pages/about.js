57509ad7cfeba5eaeb31e6cb27d016c8bee74a13
Daniyar Jakupov <ddr199305@gmail.com>
2018-11-20 22:01:00 +0100

    Add more pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About page</h1>
+    </div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/company/team.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/layout'
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>Team page</h1>
+    </div>
+  </Layout>
+)
+
+export default Team
