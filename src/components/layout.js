91ca57ae25cf5eb9986d4e8ad83685a6b7c655cc
Daniyar Jakupov <ddr199305@gmail.com>
2019-01-25 22:23:21 +0100

    Animate bg image w/ react-spring

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
+import { Spring } from 'react-spring'
 
 import Header from './header'
 import Archive from './archive'
@@ -11,7 +12,7 @@ import './layout.css'
 
 const MainLayout = styled.main`
   max-width: 70%;
-  margin: 0 auto;
+  margin: 1rem auto;
   display: grid;
   grid-template-columns: 4fr 1fr;
   grid-gap: 30px;
@@ -47,7 +48,17 @@ const Layout = ({ children, location }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <Img fluid={data.file.childImageSharp.fluid} />
+        <Spring
+          from={{ height: location.pathname === '/' ? 200 : 200 }}
+          to={{ height: location.pathname === '/' ? 300 : 100 }}
+        >
+          {styles => (
+            <div style={{ ...styles, overflow: 'hidden' }}>
+              <Img fluid={data.file.childImageSharp.fluid} />
+            </div>
+          )}
+        </Spring>
+
         <MainLayout>
           <div>{children}</div>
           <Archive />
